fb67f7bb8ad44aeea55730216b30597b796219ce
Davit Huroyan <davithuroyan@gmail.com>
2018-12-04 14:46:53 +0400

    Created NotesRepository which will implement Repository Interface and will communicate between Controllers and models

---
+++ app/Repositories/NotesRepository.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: davit
+ * Date: 04-Dec-18
+ * Time: 12:33 PM
+ */
+
+namespace App\Repositories;
+
+
+use App\Note;
+
+class NotesRepository implements RepositoryInterface
+{
+    public function __construct(Note $note)
+    {
+        $this->model = $note;
+    }
+
+    public function all($userId)
+    {
+        return $this->model->where(['user_id' => $userId])->get();
+    }
+
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function update(array $data, $id, $userId)
+    {
+        $note = $this->get($id, $userId);
+
+        $note->title = $data['title'];
+        $note->note = $data['note'];
+        return $note->save();
+    }
+
+    public function delete($id, $userId)
+    {
+        if ($this->model->where(['id' => $id, 'user_id' => $userId])->delete()) {
+            return "success";
+        }
+        return "false";
+    }
+
+    public function get($id, $userId)
+    {
+        return $this->model->where(['id' => $id, 'user_id' => $userId])->first();
+    }
+}
