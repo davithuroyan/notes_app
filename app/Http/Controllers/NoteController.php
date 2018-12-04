6372a53a40c00abb62f811f66a46637c157a9daf
Davit Huroyan <davithuroyan@gmail.com>
2018-12-04 16:10:08 +0400

    implemented create note action

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -11,6 +11,7 @@ namespace App\Http\Controllers;
 use App\Note;
 use App\Repositories\NotesRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class NoteController extends Controller
 {
@@ -18,12 +19,26 @@ class NoteController extends Controller
 
     public function __construct(Note $note)
     {
+        $this->middleware('auth');
         $this->note = new NotesRepository($note);
     }
 
     public function create(Request $request)
     {
+        $this->validate($request, [
+            'title' => 'string|required|max:50',
+            'note' => 'string|required|max:1000',
+        ]);
+        
+        $userId = Auth::user()->id;
+        $title = $request->json()->get('title');
+        $note = $request->json()->get('note');
 
+        return $this->note->create([
+            'title' => $title,
+            'note' => $note,
+            'user_id' => $userId,
+        ]);
     }
 
     public function get($noteId)
